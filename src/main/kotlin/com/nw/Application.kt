69a4ecfa78c2797d242bdc8afd992016cd82bd5e
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2023-09-30 23:24:25 +0200

    Initial commit

---
+++ src/main/kotlin/com/nw/Application.kt
@@ -0,0 +1,13 @@
+package com.nw
+
+import com.nw.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
